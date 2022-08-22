092f9b942c1fc5b31866cb8a30db14d5756764ce
theMorshed <morshedfive@gmail.com>
2022-08-22 13:47:42 +0600

    polishing some function and html element

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
+// global variable declare and initialization
 const playerList = [];
 const playerListElement = document.getElementById('player-list');
 
+// addToLeaderBoard Function while will add selected player to leader board 
 function addToLeaderBoard(event) {
     const singlePlayer = this.event.target.parentNode.parentNode;
     const playerName = singlePlayer.children[0].innerText;
+    // check player not more than 5
     if (playerList.length > 4) {
         alert('You can\'t add more than 5 player');
         return;
@@ -14,6 +17,7 @@ function addToLeaderBoard(event) {
     displayPlayer(playerList);
 }
 
+// displayPlayer function which is responsible to display player
 function displayPlayer(playerList) {
     for (let player of playerList) {
         const li = document.createElement('li');
@@ -23,8 +27,10 @@ function displayPlayer(playerList) {
     }
 }
 
+// calculate player total cost
 document.getElementById('player-cost-calculate').addEventListener('click', function () {
     const perPlayerCostField = document.getElementById('player-total-cost');
+    // getValue function invoke for getting input field value
     const perPlayerCost = getValue('player-cost');
     if (perPlayerCost !== undefined) {
         const playerCost = perPlayerCost * playerList.length;
@@ -32,11 +38,16 @@ document.getElementById('player-cost-calculate').addEventListener('click', funct
     }
 });
 
+// calculate grand total cost of whole team
 document.getElementById('total-cost-calculate').addEventListener('click', function(){
+    // getValue function invoke for getting input field value
     const managerCost = getValue('manager');
     const coachCost = getValue('coach');
     const grandTotalCostElement = document.getElementById('grand-total-cost');
+    // getValue function invoke for getting HTML element value
     const playerTotalCost = getValue('player-total-cost');
+
+    // check input value not number or NaN
     if (managerCost !== undefined && coachCost !== undefined) {
         const grandTotalCost = managerCost + coachCost + playerTotalCost;
         grandTotalCostElement.innerText = grandTotalCost;
--- js/library.js
+++ js/library.js
@@ -1,3 +1,4 @@
+// getValue function declaration
 function getValue(fieldId) {
     const element = document.getElementById(fieldId);
     if (element.tagName.toLowerCase() === 'input') {
@@ -8,7 +9,6 @@ function getValue(fieldId) {
             return undefined;
         }
         const elementValue = parseInt(elementString);
-        // element.value = '';
         return elementValue;
     } else {
         const elementString = element.innerText;
